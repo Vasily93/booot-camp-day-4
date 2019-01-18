ea46bd8b05d8b4a495eba0e956a1e2f98e771a44
Vasily93 <markovavasily@gmail.com>
2019-01-17 20:05:18 -0800

    buttons done

--- main.js
+++ main.js
@@ -2,17 +2,31 @@
 function greet(){
    
 // get the first  input by id
+    var firstNameInput = document.getElementById('first-name')
+    console. log(firstName);
+
 // get the last name by id
+    var lastNameInput = document.getElementById('last-name')
+    console. log(lastName);
 
 // get valeus out of inputs
+    var firstName= firstNameInput.value;
+    var lastName=lastNameInput.value;
+
+    console. log(firstName, lastName)
 
 // make greeting
 
+    var greeting= 'Hello ' + firstName +' '+ lastName + '!';
+    console.log(greeting);
+
 //display greeting
+
+    var greeter= document. getElementById('greeting');console. log(greeter);
+    greeter.textContent = greeting;
 
 //remove prompt
 }
 
 // TODO: validate name inputs
 
-greet();
